ed95b58512cf0c97e98615d3a11d506809d53631
Alexandre Monjol <alexandre.monjol@gmail.com>
2024-11-26 17:13:21 +0100

    misc: add toSnakeCase test scenario

--- tests/transform.test.ts
+++ tests/transform.test.ts
@@ -1,4 +1,4 @@
-import { toLowercase, toUppercase } from "../utils/transform";
+import { toLowercase, toUppercase, toSnakeCase } from "../utils/transform";
 
 describe('Transform', () => {
   describe('toUppercase', () => {
@@ -11,6 +11,12 @@ describe('Transform', () => {
     it('should convert string to lowercase', () => {
       expect(toLowercase('HELLO')).toBe('hello');
       expect(toLowercase('HOLA')).toBe('Hola');
+    });
+  })
+
+  describe('toSnakeCase', () => {
+    it('should convert string to snake case', () => {
+      expect(toSnakeCase('hello world')).toBe('hello_world');
     });
   })
 })
